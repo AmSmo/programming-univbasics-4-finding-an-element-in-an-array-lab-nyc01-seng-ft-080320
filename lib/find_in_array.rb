7ce04efe307c9de74cb5974fbb52e1e06745d1c5
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-18 18:22:34 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,7 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  index = 0
+  while index < array.length do
+    return index if value_to_find == array[index]
+    index+=1  
+  end
 end
